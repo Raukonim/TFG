7b064d73a8797b8d8047606ed65f93b2ad4a0513
Albert Fajula <fajulanet@gmail.com>
2016-03-11 14:25:21 +0100

    Added skeleton :skull:

---
+++ tfg.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 11 13:46:47 2016
+
+@author: a.fajula
+"""
+
+from __future__ import division
+from pylab import*
+
+interactive(True)
+close('all')
+
+class ElementalImage(object):
+    
+    def __init__(self):
+        something
+    
+    def load(self, path):
+        something
+
+class IntegralImage(object):
+    
+    def __init__(self):
+        something
