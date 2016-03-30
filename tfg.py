56dcae4c1ad6c8519471a05eb02ed8c225d186a1
Albert Fajula <fajulanet@gmail.com>
2016-03-30 13:50:22 +0200

    Adds normalization array function :abcd:

--- tfg.py
+++ tfg.py
@@ -15,7 +15,7 @@ close('all')
 
 class ElementalImage(object):
     
-    def __init__(self, directory, name):
+    def __init__(self, directory, name, camera):
         
         self.ei_directory = directory
         self.ei_name = name
@@ -26,9 +26,16 @@ class ElementalImage(object):
         self.image = self.load(self.ei_directory,self.ei_name)
         self.ei_pixel_x = int(shape(self.image)[0])
         self.ei_pixel_y = int(shape(self.image)[1])
+        
+        self.pixel_x = camera['sensor_x']/camera['x_pixels']
+        self.pixel_y = camera['sensor_y']/camera['y_pixels']
+        
+        self.ei_size = [self.ei_pixel_x*self.pixel_x,self.ei_pixel_y*self.pixel_y]
+
     
     def load(self, path, name):
         image = double(imread(path+name))
+        #print shape(image)
         image /= image.max()
         image = image[650:3680,650:,:]
         
@@ -41,16 +48,39 @@ class IntegralImage(object):
         self.sensor_x = camera['sensor_x']
         self.sensor_y = camera['sensor_y']
         self.sensor_ratio = camera['sensor_ratio']
+        self.sensor_array = [int(camera['sensor_array'][0]),int(camera['sensor_array'][-1])]
         
         self.ei_list = self.load_ei(ei_path)
+        
+        self.oxy = self.normalization_array()
         
     def load_ei(self, path):
         elemental_images = []
         for filename in os.listdir(path):
             #print filename
-            img = ElementalImage(path,filename)
+            img = ElementalImage(path,filename, camera)
             elemental_images.append(img)
         return elemental_images
+    
+    def normalization_array(self):
+        x_length = self.ei_list[0].ei_pixel_x
+#        print x_length
+        y_length = self.ei_list[0].ei_pixel_y
+#        print y_length
+        pixel_pitch_x = 4 / self.ei_list[0].pixel_x
+#        print pixel_pitch_x
+        pixel_pitch_y = 4 / self.ei_list[0].pixel_y
+#        print pixel_pitch_y
+        
+        normalization = zeros([self.ei_list[0].ei_pixel_x+(2*pixel_pitch_x),
+                              self.ei_list[0].ei_pixel_y+(4*pixel_pitch_y)])
+        for i in range(self.sensor_array[0]):
+            for j in range(self.sensor_array[1]):
+                normalization[i*pixel_pitch_x:(i*pixel_pitch_x)+x_length,
+                              j*pixel_pitch_y:(j*pixel_pitch_y)+y_length] += 1
+#                print i, j, normalization
+
+        return normalization
     
     def constructor(self):
         
@@ -62,15 +92,17 @@ class IntegralImage(object):
         ei_num = 0
         for ei in ei_list:
             #integral[] = 
-        
-        
+            patata        
         return integral
 
 
 camera = {
         'sensor_x' : 14.8,
         'sensor_y' : 22.2,
-        'sensor_ratio' : 3/2
+        'sensor_ratio' : 3/2,
+        'x_pixels' : 2592,
+        'y_pixels' : 3888,
+        'sensor_array' : '3x5'
 }
 
 image_directory='ei1_5x3_p4\\'
